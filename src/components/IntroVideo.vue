a526ba9e8273f2d3ccb8bfaac172e385a6d81c6d
Simon Brundin <simonbrundin@gmail.com>
2020-06-21 01:25:48 +0200

    Added youtube video and image placeholder

--- src/components/IntroVideo.vue
+++ src/components/IntroVideo.vue
@@ -1,19 +1,25 @@
 <template>
-  <div>
-    <img
-      src="https://damienrufus.com/wp-content/uploads/2016/03/video-placeholder-1.png"
-      id="video"
-      alt
-    />
+  <div id="IntroVideo">
+    <iframe
+      class="video"
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/Y8Wp3dafaMQ"
+      srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/szRqqAercTg?autoplay=1><img src=https://damienrufus.com/wp-content/uploads/2016/03/video-placeholder-1.png></a>"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
 
 <script>
-export default {};
+export default { name: "IntroVideo" };
 </script>
 
 <style scoped>
-#video {
+.video {
   height: 200px;
+  width: 100vw;
 }
 </style>
