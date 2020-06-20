07548f89fac27ebee61d06919e47d35f5d85a3fc
Simon Brundin <simonbrundin@gmail.com>
2020-06-21 01:56:36 +0200

    Added videos to video section

--- src/components/StudioPadel.vue
+++ src/components/StudioPadel.vue
@@ -1,10 +1,34 @@
 <template>
-  <div></div>
+  <div id="StudioPadel">
+    <iframe
+      width="50%"
+      src="http://www.youtube.com/embed/?listType=user_uploads&list=GoogleDevelopers&showinfo=1"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="50%"
+      src="http://www.youtube.com/embed/?listType=user_uploads&list=GoogleDevelopers&showinfo=1"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="50%"
+      src="http://www.youtube.com/embed/?listType=user_uploads&list=GoogleDevelopers&showinfo=1"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+    <iframe
+      width="50%"
+      src="http://www.youtube.com/embed/?listType=user_uploads&list=GoogleDevelopers&showinfo=1"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
 <script>
-export default {};
+export default { name: "StudioPadel" };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
