2ef3296d89c0974dc3f532238be346a86d7b672c
Simon Brundin <simonbrundin@gmail.com>
2020-06-21 01:58:56 +0200

    Added some contact info

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,10 +1,17 @@
 <template>
-  <div></div>
+  <div id="Contact">
+    <address>
+      Written by <a href="mailto:webmaster@example.com">Simon Brundin</a>.<br />
+      Visit us at:<br />
+      SuperElitSerien<br />
+      Box 564, Disneyland<br />
+      USA
+    </address>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
